70098f32f6aacefb1a6ec45e0feca367806ddd02
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-05 13:47:43 +0100

    Update script to execute correctly

--- task_1.sh
+++ task_1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+MyFirstVariable=hello
+echo $MyFirstVariable=test
+
+MySecondVariable="Hello world, can you say 'how are you?'"
+echo $MySecondVariable
+
+echo ./task_1.sh $SHELLOPS
 echo $1
-
-export MyFirstVariable=test
-
-export MySecondVariable="Hello world, can you say 'how are you?'"
 
 echo "What's up guys" >> test.txt
 
