ae8f49a7bcb3c57459d6aad8a25bec213b8e1942
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-05 14:06:31 +0100

    Add task_2 script

---
+++ task_2/task_2.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+
+//1 Find the remainder of 73 / 8.
+Test=73
+Mod=$(( $Test % 8 ))
+echo $Mod
+
+//2
+Result=((4**6) + (5**3))
+echo $Result
+
+//3
+echo ((4**6) + (5**3))
+
+//4
+if ($1 == $2) then
+echo "The same!"
+else
+echo "Not the same!"
+fi
