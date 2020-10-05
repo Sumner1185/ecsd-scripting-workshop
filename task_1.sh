1eaf361eb2aa87e36b80aef0e2fc7b4d021e55ab
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-05 13:11:27 +0100

    Remove all // from script

--- task_1.sh
+++ task_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-.
+
 export MyFirstVariable=test
 
 export MySecondVariable="Hello world, can you say 'how are you?'"
@@ -7,8 +7,6 @@ export MySecondVariable="Hello world, can you say 'how are you?'"
 export SHELLOPTS
 echo $1
 
-// 4 - Using shell command, create a file with a special character - e.g. single quote.
- touch "What's up guys"
+echo "What's up guys" >> test.txt
 
-// 5 - Remove the file you just created, using rm command. Watch escaping.
-rm What\'s\ up\ guys 
+rm -rf test.txt
