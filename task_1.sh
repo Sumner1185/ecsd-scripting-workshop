c0c8f0526ae7ede65c630a2cc66e0df47953576d
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-05 13:04:49 +0100

    Remove // from task 1, 2 and 3 in script file

--- task_1.sh
+++ task_1.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-// 1 - Define a variable.
+.
 export MyFirstVariable=test
 
-// 2 - Try to create a variable that consist of sentence that has several special characters in it. Try to output it and see how it behaves when certain characters are in there, e.g. quotes.
 export MySecondVariable="Hello world, can you say 'how are you?'"
 
-// 3 - Try to pass the shellopts environmental variable to a script and print it using echo.
 export SHELLOPTS
 echo $1
 
