de52f6d29ccaf820bca468765a305317e66e3afb
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-05 16:10:37 +0100

    As far as I got on task_5

---
+++ task_5/task_5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+// 1
+PASSWORD='MyPassword'
+echo "Please type in your password..."
+read input
+while [ $input != $PASSWORD ]; do
+echo "Please type in your password..."
+done
+
+// 2
+ARRAY=(3,4,5)
+
+for i in $ARRAY[@]}; do
+ARRAY << 
+done
